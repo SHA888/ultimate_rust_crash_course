b6093339d74e75bbd04b0d7121587a9cf4375e89
Kresna Sucandra <kresnasucandra@gmail.com>
2022-06-08 17:32:13 +0800

    Variables extra

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,15 +1,14 @@
 const STARTING_MISSILES: i32 = 8;
 const READY_AMOUNT: i32 = 2;
+// const RESULT: i32 = 0;
 
 fn main() {
-    let mut missiles = STARTING_MISSILES;
-    let ready = READY_AMOUNT;
+    let (missiles, ready) = (STARTING_MISSILES, READY_AMOUNT);
+    // READY_AMOUNT = 1;
 
     println!("Firing {} of my {} missiles...", ready, missiles);
 
-    missiles = missiles - ready;
-
-    println!("{} missiles left", missiles);
+    println!("{} missiles left", missiles - ready);
 
     
 }
